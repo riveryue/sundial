1ca6af97b54fab0fff434b86a0536d7ad080734f
yao <summerpear@foxmail.com>
2021-10-24 15:05:56 +0800

    DB

--- src/main/resources/task_conf.sql
+++ src/main/resources/task_conf.sql
@@ -1,31 +1,23 @@
-/*
- Navicat Premium Data Transfer
+CREATE database if NOT EXISTS `sundial` default character set utf8mb4 collate utf8mb4_unicode_ci;
+use `sundial`;
 
- Source Server         : local
- Source Server Type    : MySQL
- Source Server Version : 80026
- Source Host           : localhost:3306
- Source Schema         : com.umbrella
-
- Target Server Type    : MySQL
- Target Server Version : 80026
- File Encoding         : 65001
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for task_conf
 -- ----------------------------
 DROP TABLE IF EXISTS `task_conf`;
-CREATE TABLE `task_conf`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `task_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `status` tinyint(0) NULL DEFAULT NULL COMMENT '1 available, 2 unavailable',
-  `cron` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `uni_task_name`(`task_name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `task_conf` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `task_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `status` tinyint DEFAULT NULL COMMENT '1 available, 2 unavailable',
+  `cron` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uni_task_name` (`task_name`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-SET FOREIGN_KEY_CHECKS = 1;
+-- ----------------------------
+-- Records of task_conf
+-- ----------------------------
+INSERT INTO `task_conf` VALUES ('1', 'test1', '1', '*/4 * * * * ?');
+INSERT INTO `task_conf` VALUES ('2', 'test2', '1', '*/9 * * * * ?');
