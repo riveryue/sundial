4a27a10e9ac4de283ce5d6d8989ca0dd5c3645ff
yao <summerpear@foxmail.com>
2021-11-23 22:44:33 +0800

    log

--- src/main/java/sundial/Task3.java
+++ src/main/java/sundial/Task3.java
@@ -1,18 +1,21 @@
 package sundial;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import sundial.annotation.SundialTask;
 
 import java.util.Date;
 
 @Component
+@Slf4j
 public class Task3 implements SundialExecute {
 
     @SundialTask(name = "test3")
     @Override
     public void execute() {
-        Date date = new Date();
-        System.out.println(date + " three");
+
+        log.info(new Date() + " task three executed");
+
     }
 
 }
