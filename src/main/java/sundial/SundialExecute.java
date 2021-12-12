ef7f15d3bcfa7762ad3beac6946490a8845c40dd
yao <summerpear@foxmail.com>
2021-12-12 22:25:11 +0800

    zk lock

--- src/main/java/sundial/SundialExecute.java
+++ src/main/java/sundial/SundialExecute.java
@@ -1,6 +1,9 @@
 package sundial;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import sundial.annotation.SundialTask;
+import sundial.config.CuratorFrameworkConfig;
 import sundial.constant.TaskStatus;
 import sundial.dto.TaskConfDTO;
 import sundial.service.TaskConfService;
@@ -8,6 +11,7 @@ import sundial.utils.SpringUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yao
@@ -35,6 +39,25 @@ public interface SundialExecute extends Runnable {
         if (taskConfDTO != null && taskConfDTO.getStatus().equals(TaskStatus.DISABLE)) {
             return;
         }
-        execute();
+
+        String ZK_LOCK_PATH = "/distributeLock";
+        CuratorFramework client = SpringUtils.getBean(CuratorFrameworkConfig.class).curatorFramework();
+        final InterProcessMutex mutex = new InterProcessMutex(client, ZK_LOCK_PATH);
+        try {
+            //获取锁资源
+            boolean flag = mutex.acquire(1, TimeUnit.SECONDS);
+            if (flag) {
+                execute();
+            }
+        } catch (Exception e) {
+
+        } finally {
+            try {
+                mutex.release();
+            } catch (Exception e) {
+
+            }
+        }
+
     }
 }
--- src/main/java/sundial/Task2.java
+++ src/main/java/sundial/Task2.java
@@ -1,5 +1,6 @@
 package sundial;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import sundial.annotation.SundialTask;
 
@@ -9,12 +10,19 @@ import java.util.Date;
  * @author yao
  */
 @Component
+@Slf4j
 public class Task2 implements SundialExecute {
 
     @SundialTask(name = "test2")
     @Override
     public void execute() {
+        try {
+            Thread.sleep(5000L);
+        } catch (InterruptedException e) {
+            log.error("error in schedule ", e);
+        }
         System.out.println(new Date() + " task2");
+        log.info("schedule execute successfully");
     }
 
 }
