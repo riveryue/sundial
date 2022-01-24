05aa28671b3623334cf33001d6835f975ffd1060
yao <summerpear@foxmail.com>
2022-01-24 22:34:41 +0800

    zk lock config

---
+++ src/main/java/sundial/config/CuratorFrameworkConfig.java
@@ -0,0 +1,27 @@
+package sundial.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@Slf4j
+public class CuratorFrameworkConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public CuratorFramework curatorFramework() {
+        final CuratorFramework client = CuratorFrameworkFactory.newClient(env.getProperty("zkIp"), new RetryNTimes(10, 5000));
+        client.start();
+        log.info("zk status {}", client.getState());
+        return client;
+    }
+
+}
