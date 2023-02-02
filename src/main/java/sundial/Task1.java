f811baa99038af2ee0cb679e3fafd7a4bd6d2e87
yao <summerpear@foxmail.com>
2023-02-02 23:52:30 +0800

    task sleep

--- src/main/java/sundial/Task1.java
+++ src/main/java/sundial/Task1.java
@@ -14,6 +14,11 @@ public class Task1 implements SundialExecute {
     @SundialTask(name = "test1")
     @Override
     public void execute() {
+        try {
+            Thread.sleep(5000L);
+        } catch (InterruptedException e) {
+            log.error("error in schedule ", e);
+        }
         log.info("test1 execute successfully");
     }
 
